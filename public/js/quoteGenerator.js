409afcdc80a372fa1ee5530c0208e9403b6c0be3
Israel Guillermo <israelguillermo22@gmail.com>
2021-01-19 00:24:22 -0600

    logic to generate and display random quote is working

--- public/js/quoteGenerator.js
+++ public/js/quoteGenerator.js
@@ -1,8 +1,20 @@
 const searchBtn = document.getElementById('quote-finder');
 const randomQuote = document.getElementById('random-quote');
+const h1 = document.createElement('h1');
+const pTag = document.createElement('p');
 
 searchBtn.addEventListener('click', () => {
+  randomQuote.innerHTML = '';
+  h1.innerHTML = '';
+  pTag.innerHTML = '';
   fetch('https://type.fit/api/quotes')
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => {
+      let randomizedQuote = data[Math.floor(Math.random() * data.length)];
+
+      h1.append(randomizedQuote.text);
+      pTag.append(randomizedQuote.author);
+
+      randomQuote.append(h1, pTag);
+    });
 });
